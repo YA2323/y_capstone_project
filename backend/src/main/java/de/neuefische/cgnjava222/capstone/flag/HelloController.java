abd4015b3b1106749634acdf3fd99eece26234e1
yacinalkaissouni <yacin.alkaissouni@gmx.de>
2022-08-16 19:48:02 +0200

    Add Hello Controller

---
+++ backend/src/main/java/de/neuefische/cgnjava222/capstone/flag/HelloController.java
@@ -0,0 +1,15 @@
+package de.neuefische.cgnjava222.capstone.flag;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(path = "/hello")
+    String sayHello() {
+        return "Hallo";
+    }
+
+}
