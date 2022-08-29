1150d85788acb5b6e8bf0c823d4e8f996d5da962
yacinalkaissouni <yacin.alkaissouni@gmx.de>
2022-08-29 11:23:16 +0200

    Add Tests for WrongAnswers

--- backend/src/test/java/de/neuefische/cgnjava222/capstone/AnswerServiceTest.java
+++ backend/src/test/java/de/neuefische/cgnjava222/capstone/AnswerServiceTest.java
@@ -5,6 +5,10 @@ import de.neuefische.cgnjava222.capstone.answer.Answer;
 import de.neuefische.cgnjava222.capstone.answer.AnswerRepo;
 import de.neuefische.cgnjava222.capstone.answer.AnswerService;
 import de.neuefische.cgnjava222.capstone.answer.NewAnswer;
+import de.neuefische.cgnjava222.capstone.wronganswer.NewWrongAnswer;
+import de.neuefische.cgnjava222.capstone.wronganswer.WrongAnswer;
+import de.neuefische.cgnjava222.capstone.wronganswer.WrongAnswerRepo;
+import de.neuefische.cgnjava222.capstone.wronganswer.WrongAnswerService;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -52,5 +56,43 @@ class AnswerServiceTest {
         Answer actual = answerService.addAnswer(new NewAnswer("Germany"));
 
         assertThat(actual).isEqualTo(answer);
+    }
+
+
+    @Test
+    void getWrongAnswers() {
+        List<WrongAnswer> wrongAnswers = List.of(
+                new WrongAnswer("1", "Germany"),
+                new WrongAnswer("2", "Albania"),
+                new WrongAnswer("3", "France")
+        );
+
+        WrongAnswerRepo wrongAnswerRepo = mock(WrongAnswerRepo.class);
+        when(wrongAnswerRepo.findAll()).thenReturn(wrongAnswers);
+
+        WrongAnswerService wrongAnswerService = new WrongAnswerService(wrongAnswerRepo);
+
+        List<WrongAnswer> actual = wrongAnswerService.getWrongAnswers();
+        List<WrongAnswer> expected = List.of(
+                new WrongAnswer("1", "Germany"),
+                new WrongAnswer("2", "Albania"),
+                new WrongAnswer("3", "France")
+        );
+        assertThat(actual).hasSameElementsAs(expected);
+    }
+
+    @Test
+    void addWrongAnswer() {
+
+        WrongAnswer wrongAnswer = new WrongAnswer("1", "Germany");
+
+        WrongAnswerRepo wrongAnswerRepo = mock(WrongAnswerRepo.class);
+        when(wrongAnswerRepo.save(any(WrongAnswer.class)))
+                .thenReturn(wrongAnswer);
+
+        WrongAnswerService wrongAnswerService = new WrongAnswerService(wrongAnswerRepo);
+        WrongAnswer actual = wrongAnswerService.addWrongAnswer(new NewWrongAnswer("Germany"));
+
+        assertThat(actual).isEqualTo(wrongAnswer);
     }
 }
