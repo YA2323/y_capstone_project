b6a12f611788601c1f4ded25fa0d3b0b32693963
yacinalkaissouni <yacin.alkaissouni@gmx.de>
2022-09-05 01:00:34 +0200

    Change Tab Title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import React, {} from 'react';
+import React, {useEffect} from 'react';
 import './style/App.css';
 import Header from "./components/Header";
 import AllRoutes from "./components/AllRoutes";
 import {HashRouter} from "react-router-dom";
 
 function App() {
+
+    useEffect(() => {
+        document.title = "GUESS THE FLAG"
+    },[])
 
     return (
         <>
