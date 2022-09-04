38437155936e9c64479becb8a905172b0d26d724
yacinalkaissouni <yacin.alkaissouni@gmx.de>
2022-09-04 23:46:28 +0200

    Add IntegrationTest for RandomFlags

---
+++ backend/src/test/java/de/neuefische/cgnjava222/capstone/randomflag/EuropeFlagServiceTest.java
@@ -0,0 +1,18 @@
+package de.neuefische.cgnjava222.capstone.randomflag;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EuropeFlagServiceTest {
+
+    @Test
+    void getRandomEuropeCountries() {
+
+        List<String> europeCountries = Arrays.asList("Albania", "Andorra",
+                "Austria", "Azerbaijan", "Belarus");
+    }
+}
