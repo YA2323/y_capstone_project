e98fd0754cf6338d7cd5f9a418e39def1f2ebdae
yacinalkaissouni <yacin.alkaissouni@gmx.de>
2022-09-05 00:37:55 +0200

    Add Shuffle for answer Buttons

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -10,13 +10,25 @@ export default function Game() {
         getRandomEuroFlags()
     }
 
+    const flagAnswers: string[] = [randomFlags.rightFlag,randomFlags.wrongFlagOne,randomFlags.wrongFlagTwo,randomFlags.wrongFlagThree]
+
+    const shuffle = ([...arr]) => {
+        let m = arr.length;
+        while (m) {
+            const i = Math.floor(Math.random() * m--);
+            [arr[m], arr[i]] = [arr[i], arr[m]];
+        }
+        return arr;
+    }
+    const randomFlagAnswers = shuffle(flagAnswers)
+
     return (
 
         <>
-            <Button id={"b1"}>{randomFlags.rightFlag}</Button>
-            <Button id={"b2"}>{randomFlags.wrongFlagOne}</Button>
-            <Button id={"b3"}>{randomFlags.wrongFlagTwo}</Button>
-            <Button id={"b4"}>{randomFlags.wrongFlagThree}</Button>
+            <Button id={"b1"}>{randomFlagAnswers.at(0)}</Button>
+            <Button id={"b2"}>{randomFlagAnswers.at(1)}</Button>
+            <Button id={"b3"}>{randomFlagAnswers.at(2)}</Button>
+            <Button id={"b4"}>{randomFlagAnswers.at(3)}</Button>
             <Button id={"b0"} onClick={handleNeu}>NEXT FLAG</Button>
             <div className={"imgFlag"}>
                 <img src={randomFlags.rightFlagUrl} alt={"CLICK TO START THE GAME!"}/>
