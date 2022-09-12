af8f7fe318e44e0cb6a8f6f6bec8ab878314b91b
yacinalkaissouni <yacin.alkaissouni@gmx.de>
2022-09-12 13:11:31 +0200

    Update Delete Answer Button

--- frontend/src/components/GameHistory.tsx
+++ frontend/src/components/GameHistory.tsx
@@ -1,6 +1,6 @@
 import {Answer} from "./Answer";
-import DeleteAnswers from "./DeleteAnswers";
 import "../style/GameHistory.css"
+import {Button} from "@mui/material";
 
 type gameHistoryProps = {
     allAnswers: Answer[],
@@ -10,13 +10,19 @@ type gameHistoryProps = {
 
 export default function GameHistory(props: gameHistoryProps) {
 
+    const handleDeleteClick = () => {
+        if (props.allAnswers.length > 0) {
+            props.deleteAllAnswers(props.allAnswers[0].id)
+        }
+    }
 
     return (
         <>
             <div className={"answerHistory"}>
                 <h2>GAME HISTORY</h2>
-                {props.allAnswers.map((element) => <li key={element.id}> {element.answerTrueOrFalse} {element.points}
-                    <DeleteAnswers answer={element} deleteAllAnswers={props.deleteAllAnswers}/></li>)}
+                {props.allAnswers.map((element) => <li
+                    key={element.id}> {element.answerTrueOrFalse} {element.points}</li>)}
+                <Button id={"deleteBtn"} onClick={handleDeleteClick}>CLEAR ALL</Button>
             </div>
         </>
     )
